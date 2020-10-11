13cf0c4355d56d3e95ddd1150c90311f1d1a6640
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-10-12 01:37:04 +0200

    Build all Feature Components with Styles & add it to HeaderCompound

--- compounds/HeaderCompound.js
+++ compounds/HeaderCompound.js
@@ -1,8 +1,12 @@
 import React from "react";
+import FeatureWrapper from "../components/Feature/FeatureWrapper";
 import HeaderWrapper from "../components/Header/HeaderWrapper";
 import Logo from "../components/Header/Logo";
 import Navbar from "../components/Header/Navbar";
 import SigninButton from "../components/Header/SigninButton";
+import FeatureTitle from "../components/Feature/FeatureTitle";
+import FeatureSubTitle from "../components/Feature/FeatureSubTitle";
+
 
 function HeaderCompound({ children }) {
   return (
@@ -12,6 +16,10 @@ function HeaderCompound({ children }) {
           <Logo />
           <SigninButton>Sign In</SigninButton>
         </Navbar>
+        <FeatureWrapper>
+          <FeatureTitle>Unlimited movies, TV shows and more.</FeatureTitle>
+          <FeatureSubTitle>Watch anywhere. Cancel at any time.</FeatureSubTitle>
+        </FeatureWrapper>
       </HeaderWrapper>
     </>
   );
