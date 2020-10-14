cb3317b638c799e68b81f9d4d0a89f1ff9608070
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-10-14 04:54:51 +0200

    Move Head Info to _document.js file

--- pages/_document.js
+++ pages/_document.js
@@ -31,7 +31,18 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <title>Netflix NEXT.JS</title>
+          <meta charset="utf-8" />
+          <meta
+            name="description"
+            content="Netflix Clone Built Using NEXT.JS"
+          />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
