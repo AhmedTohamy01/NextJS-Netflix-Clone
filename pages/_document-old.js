dc4af1a05748c4dd7111b1065902b387d385d487
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-10-14 04:48:55 +0200

    Update _document.js

--- pages/_document-old.js
+++ pages/_document-old.js
@@ -1,4 +1,4 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
@@ -26,17 +26,5 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
-  }
-
-  render() {
-    return (
-      <Html lang="en">
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
   }
 }
