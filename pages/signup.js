6360c137df230ee855669933d19e36814d93afc6
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-10-15 02:33:13 +0200

    Add Signup Page with all styles (Firebase Function Still Not Working)

---
+++ pages/signup.js
@@ -0,0 +1,96 @@
+import React, { useState, useContext } from "react";
+import { useRouter } from "next/router";
+import { FirebaseContext } from "../context/FirbaseContext";
+import HeaderWrapper from "../components/Header/HeaderWrapper";
+import Navbar from "../components/Header/Navbar";
+import Logo from "../components/Header/Logo";
+import FooterCompound from "../compounds/FooterCompound";
+import SignFormWrapper from "../components/SignForm/SignFormWrapper";
+import SignFormBase from "../components/SignForm/SignFormBase";
+import SignFormTitle from "../components/SignForm/SignFormTitle";
+import SignFormInput from "../components/SignForm/SignFormInput";
+import SignFormButton from "../components/SignForm/SignFormButton";
+import SignFormText from "../components/SignForm/SignFormText";
+import SignFormLink from "../components/SignForm/SignFormLink";
+import SignFormCaptcha from "../components/SignForm/SignFormCaptcha";
+import SignFormError from "../components/SignForm/SignFormError";
+
+function SignupPage() {
+  const history = useRouter();
+  const firebase = useContext(FirebaseContext);
+
+  const [firstName, setFirstName] = useState("");
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const IsInvalid = password === "" || emailAddress === "" || firstName === "";
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(emailAddress, password)
+      .then((result) =>
+        result.user
+          .updateProfile({
+            displayName: firstName,
+          })
+          .then(() => {
+            setFirstName("");
+            setEmailAddress("");
+            setPassword("");
+            history.push("/browse");
+          })
+      )
+      .catch((error) => setError(error.message));
+  }
+
+  return (
+    <>
+      <HeaderWrapper>
+        <Navbar>
+          <Logo />
+        </Navbar>
+        <SignFormWrapper>
+          <SignFormBase onSubmit={handleSubmit} method="POST">
+            <SignFormTitle>Sign Up</SignFormTitle>
+            {error ? <SignFormError>{error}</SignFormError> : null}
+            <SignFormInput
+              type="text"
+              placeholder="First Name"
+              value={firstName}
+              onChange={({ target }) => setFirstName(target.value)}
+            />
+            <SignFormInput
+              type="text"
+              placeholder="Email Address"
+              value={emailAddress}
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <SignFormInput
+              type="password"
+              placeholder="Password"
+              autoComplete="off"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <SignFormButton disabled={IsInvalid}>Sign Up</SignFormButton>
+            <SignFormText>
+              Already a user?
+              <SignFormLink href="/signin">Sign in now.</SignFormLink>
+            </SignFormText>
+            <SignFormCaptcha>
+              This page is protected by Google reCAPTCHA to ensure you are not a
+              bot.
+            </SignFormCaptcha>
+          </SignFormBase>
+        </SignFormWrapper>
+      </HeaderWrapper>
+      <FooterCompound />
+    </>
+  );
+}
+
+export default SignupPage;
